ec10fe2d98b45507bc552547605c6006ff4c712e
Vaibhav Dewdhe <108789227+VaibhavDewdhe@users.noreply.github.com>
2024-12-17 14:03:42 +0530

    get back

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   const {theme} = useContext(ThemeContext)
   return (
       <div className={`App ${theme}`}>
-        <h1>Hello Shubham sir</h1>
         <TaskApp />
       </div>
   );
